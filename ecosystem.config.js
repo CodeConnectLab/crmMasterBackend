f32450603c1abfdd132b4b86fa34a9b58768754c
bittusing <abhilekhk6@gmail.com>
2024-12-05 12:42:47 +0530

    "Updated ecosystem.config.js to add environment variables and logging configuration"

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,25 @@
+// module.exports = {
+// 	apps: [
+// 		{
+// 			name: "app",
+// 			script: "./app.js",
+// 		},
+// 	],
+// };
+
 module.exports = {
-	apps: [
-		{
-			name: "app",
-			script: "./app.js",
-		},
-	],
-};
+	apps: [{
+	  name: "Backend",
+	  script: "./server/app.js",
+	  env: {
+		NODE_ENV: "production",
+		PORT: 9000,
+		HOST: "http://13.203.123.47:9000",  // Update with your EC2 IP
+		MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://87659/9:9869586789@cluster0.ocxugzh.mongodb.net/mydatabase1?retryWrites=true&w=majority',
+		SESSION_SECRET: process.env.SESSION_SECRET || 'your_jwt_secret',
+		REFRESH_SECRET: process.env.REFRESH_SECRET || 'your_refresh_jwt_secret'
+	  },
+	  error_file: "./logs/err.log",
+	  out_file: "./logs/out.log",
+	}]
+  }
